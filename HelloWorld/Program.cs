ec59eff7cc2b36a37a414e60cef8818872627d2f
RClOwen <rco201@email.vccs.edu>
2022-08-24 19:25:47 -0400

    Week 1 Assignment and getting my feet wet with syntax.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿//Hello World! Program to get my feet wet with the syntax.
+//Fairly rusty on C# stuff
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(String[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week1-Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Week1_Variables
+{
+    class Week1
+    {
+        static void Main(String[] args)
+        {
+            System.Console.WriteLine("Who is being seen today?");
+            string patientName = Console.ReadLine();
+            System.Console.WriteLine("Welcome: " + patientName);
+            Console.ReadKey();
+        }
+    }
+}
